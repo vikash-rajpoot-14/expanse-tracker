0b5bd3728f3b673797eb78bbc1e48e81993bfd34
vikash rajpoot <vikashraj1490@gmail.com>
2023-04-25 00:36:02 +0530

    error fix

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB, "root", process.env.PASSWORD, {
   dialect: "mysql",
-  host: "localhost",
+  host: "3.87.145.123",
 });
 
 module.exports = sequelize;
